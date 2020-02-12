95575054aaecc71caa6372c557edeef68896f3d1
Rotem <tetch6@gmail.com>
2020-02-12 11:16:13 +0200

    added images

--- js/meme-controller.js
+++ js/meme-controller.js
@@ -1,3 +1,45 @@
 'use strict';
 
 console.log('controller');
+
+var gCanvas;
+var gCtx
+
+
+function onInit(){
+    gCanvas = document.getElementById('meme-canvas');
+    gCtx = gCanvas.getContext('2d');
+    showImages()
+}
+
+
+
+
+
+// --- IMAGE GALLERY --- 
+
+    var imgGallery = getImageGallery()
+    function showImages(){
+        var strHTMLs = 
+        imgGallery.map(img => {
+          return  `<img src"${img.url}"/>`
+        }).join('');
+        var elGallery = document.querySelector('.img-gallery');
+        // elGallery.innerHTML = strHTMLs;
+        console.log('gallery', elGallery);
+
+    }
+
+
+
+
+// --- MEME EDITOR ---
+
+function darwImg(){
+    var currImg = getImg(0);
+    var img = new Image();
+    img.src = currImg.url
+    img.onload = () => {
+        gCtx.drawImage(img, 0, 0, gCanvas.width, gCanvas.height)
+    }
+}
--- js/meme-service.js
+++ js/meme-service.js
@@ -1,3 +1,56 @@
 'use strict';
 
 console.log('service');
+
+// --- GLOBALS ---
+var gImgs = [
+    {
+        id: 0,
+        url: '.\/img\/1.jpg',
+        keywords:['funny']
+    },
+    {
+        id: 1,
+        url: '.\/img\/2.jpg',
+        keywords:['cute']
+    },
+]
+
+var gMeme = {
+    selectedImgId:0,
+    selectedLineidx:0,
+
+    lines:[
+        {
+            txt: 'Lorem ipsum dolor blah',
+            size: 20,
+            align: 'left',
+            color: 'red' 
+        },
+    ],
+}
+
+
+// --- IMAGE GALLERY ---
+
+function getImageGallery(){
+    return gImgs
+}
+
+// TODO - Hold array of image objects
+// TODO - Every image has an id, url, keyword
+// TODO - 
+
+// --- SEARCH ---
+
+// TODO - Array of keywords objects contains keyword and num of times it've been search
+// 
+
+
+
+// --- MEME EDITOR ---
+
+function getImg(selectedImg){
+    return gImgs[selectedImg];
+}
+// TODO - hold the x,y position of the text box
