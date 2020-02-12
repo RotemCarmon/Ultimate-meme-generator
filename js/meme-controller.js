0fbaf50e9594078692b7636b859abc8ca5861f34
Rotem <tetch6@gmail.com>
2020-02-12 12:05:26 +0200

    onclick update modal & draw img

--- js/meme-controller.js
+++ js/meme-controller.js
@@ -6,7 +6,7 @@ var gCanvas;
 var gCtx
 
 
-function onInit(){
+function onInit() {
     gCanvas = document.getElementById('meme-canvas');
     gCtx = gCanvas.getContext('2d');
     showImages()
@@ -18,28 +18,44 @@ function onInit(){
 
 // --- IMAGE GALLERY --- 
 
+function showImages() {
     var imgGallery = getImageGallery()
-    function showImages(){
-        var strHTMLs = 
+    var strHTMLs =
         imgGallery.map(img => {
-          return  `<img src"${img.url}"/>`
+            return `<img src="${img.url}" alt="" data-img="${img.id}" onclick="onImgSelect(this)">`
         }).join('');
-        var elGallery = document.querySelector('.img-gallery');
-        // elGallery.innerHTML = strHTMLs;
-        console.log('gallery', elGallery);
+    var elGallery = document.querySelector('.img-gallery');
+    elGallery.innerHTML = strHTMLs;
+    // console.log('gallery', elGallery);
 
-    }
+}
+
+function onImgSelect(img) {
+    var selectedImgId = img.dataset.img;
+    updateCurrImgId(selectedImgId)
+    onDarwImg()
+    // return selectedImgId;    
+}
 
 
 
 
 // --- MEME EDITOR ---
 
-function darwImg(){
-    var currImg = getImg(0);
+function onDarwImg() {
+    var currImg = getImg();
     var img = new Image();
     img.src = currImg.url
     img.onload = () => {
         gCtx.drawImage(img, 0, 0, gCanvas.width, gCanvas.height)
     }
+}
+
+function onAddText() {
+    var firstTextLine = getText();
+    gCtx.font = '40px IMPACT';
+    gCtx.fillStyle = '#ffffff'
+    gCtx.strokeStyle = '#000000'
+    gCtx.strokeText(firstTextLine, 50, 80)
+    gCtx.fillText(firstTextLine, 50, 80)
 }
--- js/meme-service.js
+++ js/meme-service.js
@@ -3,15 +3,18 @@
 console.log('service');
 
 // --- GLOBALS ---
+
+// TODO - Hold array of image objects
 var gImgs = [
+    // TODO - Every image has an id, url, keyword
     {
         id: 0,
-        url: '.\/img\/1.jpg',
+        url: './img/1.jpg',
         keywords:['funny']
     },
     {
         id: 1,
-        url: '.\/img\/2.jpg',
+        url: './img/2.jpg',
         keywords:['cute']
     },
 ]
@@ -22,7 +25,7 @@ var gMeme = {
 
     lines:[
         {
-            txt: 'Lorem ipsum dolor blah',
+            txt: 'Fuck You All',
             size: 20,
             align: 'left',
             color: 'red' 
@@ -31,26 +34,31 @@ var gMeme = {
 }
 
 
+function updateCurrImgId(id){
+    gMeme.selectedImgId = id
+
+}
+
 // --- IMAGE GALLERY ---
 
 function getImageGallery(){
     return gImgs
 }
 
-// TODO - Hold array of image objects
-// TODO - Every image has an id, url, keyword
-// TODO - 
+// --- MEME EDITOR ---
+
+function getImg(){
+    return gImgs[gMeme.selectedImgId];
+}
+
+
+function getText(){
+    console.log(gMeme.lines)
+    return gMeme.lines[0].txt.toUpperCase();
+}
+// TODO - hold the x,y position of the text box
+
 
 // --- SEARCH ---
 
 // TODO - Array of keywords objects contains keyword and num of times it've been search
-// 
-
-
-
-// --- MEME EDITOR ---
-
-function getImg(selectedImg){
-    return gImgs[selectedImg];
-}
-// TODO - hold the x,y position of the text box
